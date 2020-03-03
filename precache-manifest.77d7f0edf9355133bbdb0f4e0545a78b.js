ee525fccfe0a647eeb5450e5f3649761c35604c5
papp.norbert <npapp@ict.ag>
2020-03-03 14:29:47 +0100

    Initial commit

---
+++ precache-manifest.77d7f0edf9355133bbdb0f4e0545a78b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea36309c7efad2f0fcbae578642b662a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e9ad4b84a84a6648390",
+    "url": "/static/css/main.d0d977e5.chunk.css"
+  },
+  {
+    "revision": "18ec4b52431f4b81b397",
+    "url": "/static/js/2.70b97244.chunk.js"
+  },
+  {
+    "revision": "74b5b99a348fedd13ab1cb018bec8748",
+    "url": "/static/js/2.70b97244.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e9ad4b84a84a6648390",
+    "url": "/static/js/main.54cd53ae.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  }
+]);
